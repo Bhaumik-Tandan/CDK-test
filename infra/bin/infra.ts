b48149fa7346f168f47656b802cf7598f7b982a9
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-11-16 02:24:58 +0530

    [add] github actions for generic branch

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,7 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
+const branchName=process.env.BRANCH_NAME||'master';
+new InfraStack(app, 'InfraStack'+branchName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -4,12 +4,14 @@ import { CfnOutput } from 'aws-cdk-lib';
 import { Bucket, BlockPublicAccess, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
+const branchName=process.env.BRANCH_NAME||'master';
 export class InfraStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         // Create an S3 bucket for hosting the React application
         const bucket = new Bucket(this, 'ReactAppBucket', {
+            bucketName: `react-app-bucket-${branchName}`, 
             websiteIndexDocument: 'index.html', // Main entry point for the website
             websiteErrorDocument: 'index.html',  // Redirect to index.html on error (for client-side routing)
             publicReadAccess: true,               // Allow public access to the bucket
